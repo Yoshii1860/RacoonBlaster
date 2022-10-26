a80798df7cbf2dc2e48d6fe4464e5501d9959068
Yoshii1860 <yoshii1860@gmail.com>
2022-10-26 17:52:38 -0500

    Tower Prefab & Target Enemy

--- Assets/Tiles/Waypoint.cs
+++ Assets/Tiles/Waypoint.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    [SerializeField] GameObject newTile;
+    [SerializeField] GameObject archerPrefab;
     [SerializeField] bool isPlaceable;
 
     void OnMouseDown() 
     {
         if(isPlaceable)
         {
-        Debug.Log(transform.name);
+        Instantiate(newTile, transform.position, Quaternion.identity);
+        Instantiate(archerPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
         }
     }
 }
