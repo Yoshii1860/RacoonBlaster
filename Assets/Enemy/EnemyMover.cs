64bc12ae9dc51189ff86605946d314b01848be77
Yoshii1860 <yoshii1860@gmail.com>
2022-10-26 13:57:10 -0500

    Tile Prefabs & Enemy Movement (workaround rotation)

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float waitTime = 1f;
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
     void Start()
     {
+        transform.position = path[0].transform.position;
         StartCoroutine(FollowPath()); 
     }
 
@@ -16,8 +17,19 @@ public class EnemyMover : MonoBehaviour
         foreach(Waypoint waypoint in path) 
         {
             {
-                transform.position = waypoint.transform.position;
-                yield return new WaitForSeconds(waitTime);
+                Vector3 startPosition = transform.position;
+                Vector3 endPosition = waypoint.transform.position;
+                float travelPercent = 0f;
+                
+                transform.LookAt(endPosition);
+                transform.Rotate(0,90,0);
+
+                while(travelPercent <1f)
+                {
+                    travelPercent += Time.deltaTime * speed;
+                    transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                    yield return new WaitForEndOfFrame();
+                }
             }
         }
     }
