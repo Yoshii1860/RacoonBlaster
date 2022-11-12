a6b6a44d61c1f52102095796bfe82cdb8703a94e
Yoshii1860 <yoshii1860@gmail.com>
2022-11-12 10:32:10 -0600

    Build Timer

--- Assets/Tower/Tower.cs
+++ Assets/Tower/Tower.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    [SerializeField] float buildDelay = 1f;
+    void Start() 
+    {
+        StartCoroutine(Build());
+    }
 
     public bool CreateTower(Tower tower, Vector3 position)
     {
@@ -22,5 +27,27 @@ public class Tower : MonoBehaviour
         }
 
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach(Transform child in transform) 
+        {
+            child.gameObject.SetActive(false);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach(Transform child in transform) 
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildDelay);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
     }
 }
